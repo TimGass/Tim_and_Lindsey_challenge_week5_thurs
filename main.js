b62d416bae8ae49109b0f57395967d58d00ba77c
Timothy Andrew Gass <timothyagass@gmail.com>
2015-11-05 09:36:22 -0600

    should be good

---
+++ main.js
@@ -0,0 +1,27 @@
+function isValid(numberString){
+  var numberArray = numberString.replace(/ /g, "").split('');
+
+  for(x = 0; x < numberArray.length; x++){
+    if(x%2 === 0)
+      numberArray[x] = (Number(numberArray[x]) * 2);
+    else
+      numberArray[x] = Number(numberArray[x]);
+  }
+
+  var answer = numberArray.reduce(function(total, current){
+    return total + current;
+  });
+
+  if (answer % 10 === 0) {
+    return true;
+  }
+
+  else{
+    return false;
+  }
+}
+
+
+console.assert(isValid("4408 0412 3456 7893") === false);
+console.assert(isValid("5000 0000 0000 0000") === false);
+console.assert(isValid("4408 0412 3456 2193") === true);
